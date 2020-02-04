6dc88abda66577db3175cf90ccffed9f4255ece1
Walter Prata <walterasprata@gmail.com>
2020-02-04 20:15:25 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/projetograal/ProjetoGraalApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetograal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGraalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGraalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetograal/ProjetoGraalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetograal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGraalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
